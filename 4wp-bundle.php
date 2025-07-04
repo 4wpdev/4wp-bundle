6ebca6d10775b91cb36298662eadc5d076d615e3
4wp-dev <4wpdev@gmail.com>
2025-07-04 16:37:49 +0300

    Create 4wp-bundle.php

---
+++ 4wp-bundle.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Plugin Name: 4WP Bundle
+ * Description: Core bundle to support and manage 4WP Blocks ecosystem.
+ * Version: 1.0.6
+ * Author: 4wp.dev
+ * Author URI: https://4wp.dev
+ * Text Domain: 4wp-bundle
+ * Domain Path: /languages
+ * Requires at least: 5.0
+ * Tested up to: 6.4
+ * Requires PHP: 7.4
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Network: false
+ */
